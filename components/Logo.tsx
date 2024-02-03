8a905e98acfd4cd6edd8ba7c00203b1ba0185eb3
north-tower <muneshmyke@gmail.com>
2024-02-03 14:01:20 +0300

    6th

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link"
+import { AspectRatio } from "./ui/aspect-ratio"
+import Image from "next/image"
 
 function Logo() {
   return (
-    <Link href="/">Go Home</Link>
-  )
+    <Link href="/" className="overflow-hidden">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio ratio={16 / 9} className="flex items-center justify-center">
+          <Image priority src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w" alt="logo" className="dark:filter dark:invert" />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
 }
 
 export default Logo
