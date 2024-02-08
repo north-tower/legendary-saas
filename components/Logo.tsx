6c84c703ee1308fed54477a4ecc722f0136dcfc8
north-tower <muneshmyke@gmail.com>
2024-02-08 17:17:13 +0300

    9th

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ function Logo() {
     <Link href="/" prefetch={false} className="overflow-hidden">
       <div className="flex items-center w-72 h-14">
         <AspectRatio ratio={16 / 9} className="flex items-center justify-center">
-          <Image priority width={30} height={30} src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w" alt="logo" className="dark:filter dark:invert" />
+          <Image priority width={31} height={30} src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w" alt="logo" className="dark:filter dark:invert" />
         </AspectRatio>
       </div>
     </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config = {
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        ring: "hsl(var(--ring))",  
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
