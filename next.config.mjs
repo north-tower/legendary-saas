884782b661803b34a3a4f80a569fc59e20194401
north-tower <muneshmyke@gmail.com>
2024-02-09 15:12:51 +0300

    12th

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.example.com',
-        port: '',
-        pathname: '/account123/**',
-      },
-    ],
-  },
-}
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
