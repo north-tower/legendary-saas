18fcd33f67086216679be899b75f7c3a7fe659c0
north-tower <muneshmyke@gmail.com>
2024-02-16 13:57:11 +0300

    007

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="">
       <div className="relative isolate pt-14 dark:bg-gray-900">
-        <div>
-
+        <div className="absolute inset-x-0 top-28 -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
+          <div className="relative left-[calc[50%-11rem]] aspect-[1155/678 w-[36,125rem]"
         </div>
       </div>
     </main>
